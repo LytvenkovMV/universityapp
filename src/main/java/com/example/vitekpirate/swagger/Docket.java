e0ee9e86d570e2bd7c651262a3d3ff57e76c15e9
LytvenkovMV <lytvenkovmv@gmail.com>
2023-06-08 10:52:54 +0300

    Изменены аннотации касательно swagger

--- src/main/java/com/example/vitekpirate/swagger/Docket.java
+++ src/main/java/com/example/vitekpirate/swagger/Docket.java
@@ -1,13 +1,16 @@
 package com.example.vitekpirate.swagger;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Component
+@EnableSwagger2
 public class Docket {
 
+    @Bean
     public springfox.documentation.spring.web.plugins.Docket api() {
         return new springfox.documentation.spring.web.plugins.Docket(DocumentationType.SWAGGER_2)
                 .select()
--- src/test/java/com/example/vitekpirate/VitekpirateApplicationTests.java
+++ src/test/java/com/example/vitekpirate/VitekpirateApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.vitekpirate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootTest
+@EnableSwagger2
 class VitekpirateApplicationTests {
 
     @Test
